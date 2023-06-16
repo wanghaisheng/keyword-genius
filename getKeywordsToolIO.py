173279315274d1dc029b5b52fa503275b8e07588
HeisenBerg? <edwin_uestc@163.com>
2023-06-16 11:05:08 +0800

    Create getKeywordsToolIO.py

---
+++ getKeywordsToolIO.py
@@ -0,0 +1,15 @@
+# POST
+# 	https://keywordtool.io/search/keywords/youtube/metrics?signature=ac32cb4482b8bc71efd92906562e1a3b594ad35b6fc271116331b6e2a0ada0c6
+# Accept
+# 	application/json, text/plain, */*
+# Accept-Encoding
+# 	gzip, deflate, br
+# Accept-Language
+# 	en-US,en;q=0.5
+# Connection
+# 	keep-alive
+# Content-Length
+# 	167
+# Content-Type
+# 	application/json
+
